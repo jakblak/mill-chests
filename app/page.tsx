bec2519edc169e5b66dc69e0b07c0dd29c5d2ccb
jakblak <schroe9@gmail.com>
2024-08-06 18:59:24 -0700

    fix infinity scroll error

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ import { useMutation, useQuery } from "convex/react";
 import { FixedSizeGrid as Grid } from "react-window";
 import { useMeasure } from "react-use";
 
-const NUMBER_OF_CHESTS = 10000;
+const NUMBER_OF_CHESTS = 1_000_000;
 const ROW_HEIGHT = 100;
 const COLUMN_WIDTH = 100;
+const LEFT_PADDING = 10; // New constant for left padding
+const RIGHT_PADDING = 1; // New constant for right padding
 
 function Chest({ rowIndex,
   columnIndex,
@@ -52,26 +54,29 @@ function Chest({ rowIndex,
 export default function Home() {
   const openBoxSum = useQuery(api.sums.getOpenBoxSum) ?? 0;
   const [ref, { width, height }] = useMeasure<HTMLDivElement>();
-  console.log(height);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center h-full">
       <h1 className="text-4xl mb-4">One Million Treasure Chests</h1>
       <p className="text-2xl mb-4">{openBoxSum} of 1,000,000 chests opened</p>
-      <div ref={ref} className="w-full h-screen">
-        <Grid
-          columnCount={Math.floor(width / COLUMN_WIDTH)}
-          columnWidth={COLUMN_WIDTH}
-          height={height}
-          width={width}
-          rowCount={NUMBER_OF_CHESTS / Math.ceil(width / COLUMN_WIDTH)}
-          rowHeight={ROW_HEIGHT}
-          itemData={{
-            columnCount: Math.ceil(width / COLUMN_WIDTH),
-          }}
-        >
-          {Chest}
-        </Grid>
+      <div ref={ref} className="w-full h-screen" style={{ paddingLeft: LEFT_PADDING, paddingRight: RIGHT_PADDING }}>
+        {width && height ? (
+          <Grid
+            columnCount={Math.floor((width - LEFT_PADDING - RIGHT_PADDING) / COLUMN_WIDTH)}
+            columnWidth={COLUMN_WIDTH}
+            height={height}
+            width={width - LEFT_PADDING - RIGHT_PADDING}
+            rowCount={Math.ceil(NUMBER_OF_CHESTS / Math.floor((width - LEFT_PADDING - RIGHT_PADDING) / COLUMN_WIDTH))}
+            rowHeight={ROW_HEIGHT}
+            itemData={{
+              columnCount: Math.floor((width - LEFT_PADDING - RIGHT_PADDING) / COLUMN_WIDTH),
+            }}
+          >
+            {Chest}
+          </Grid>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </main>
   );
